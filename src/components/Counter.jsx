0465a061a5d8f823fef0464fc9bcaccbd38b54d6
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-04-20 21:30:01 +0530

    New Redux Project

---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { decrement, increment } from "./action";
+import { connect } from "react-redux";
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <div>
+      <h2>Count: {count} </h2>
+      <button onClick={increment}>Increment</button>
+      {count > 0 && <button onClick={decrement}>Decrement</button>}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+const mapDispatchToProps = {
+  increment,
+  decrement,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
